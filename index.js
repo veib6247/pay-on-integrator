efcc17dfd9c5aa98ecc094e53414c2a77ba138f9
Bryan Olandres <ozoneblacklight@outlook.com>
2022-03-18 22:07:26 +0800

    support for Standalone 3DS

--- index.js
+++ index.js
@@ -4,23 +4,49 @@ import fetch from 'node-fetch'
  */
 const integrator = {
   /**
-   * Submits your data to generate a checkout ID
+   * Submits the data to the appropriate endpoint depending on the integrationType provided by the user.
+   *
+   * CopyAndPay - Returns a checkout ID that you will need to call the widget.
+   * ServerToServer - Support synchronous transactions only, does not autodirect to URL from the intermediate response.
+   * threeDSecure - Standalone 3D Secure transaction request. Only returns intermediate response, does not auto-redirect.
+   *
+   * @param {string} integrationType The type of integration you want to send the request to. Available options are: ["CopyAndPay", "ServerToServer", "threeDSecure"].
    * @param {boolean} isTestMode Determines if you want to hit the test or live environment.
    * @param {string} accessToken Your API token.
    * @param {string} parameters Your parameters, must be in query string parameters format.
    * @returns Promise JSON response object. Needs to be fullfilled.
    */
-  submitCopyandPay: async (isTestMode, accessToken, parameters) => {
-    // init endPoint var to blank string
-    let endPoint = ''
-
-    // eval mode
+  submitTransactionRequest: async (
+    integrationType,
+    isTestMode,
+    accessToken,
+    parameters
+  ) => {
+    // init sub domain
+    let subDomain = ''
     if (isTestMode) {
-      endPoint = 'https://eu-test.oppwa.com/v1/checkouts'
+      subDomain = 'eu-test'
     } else {
-      endPoint = 'https://eu-prod.oppwa.com/v1/checkouts'
+      subDomain = 'eu-prod'
     }
 
+    // init URL endpoint
+    let endPoint = ''
+    switch (integrationType) {
+      case 'CopyAndPay':
+        endPoint = `https://${subDomain}.oppwa.com/v1/checkouts`
+        break
+      case 'ServerToServer':
+        endPoint = `https://${subDomain}.oppwa.com/v1/payments`
+        break
+      case 'threeDSecure':
+        endPoint = `https://${subDomain}.oppwa.com/v1/threeDSecure`
+        break
+      default:
+        break
+    }
+
+    // le fetch
     const rawResponse = await fetch(endPoint, {
       method: 'POST',
       headers: {
@@ -33,7 +59,6 @@ const integrator = {
     // return response, to be fullfilled
     return rawResponse.json()
   },
-
   /**
    * Fetches the transaction result after CopyandPay redirects to the shopperResultURL.
    * @param {boolean} isTestMode Determines if you want to hit the test or live environment.
