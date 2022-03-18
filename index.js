e64d2b2a35ac4bc24567bac75ec691eb3fd36f62
Bryan Olandres <ozoneblacklight@outlook.com>
2022-03-18 18:38:05 +0800

    added node-fetch as dependency, added function to support copyandpay

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
+import fetch from 'node-fetch'
 /**
- * takes in the type of integration and parameters
- * @param {string} integrationMode
+ * main object to handle most of the operations
  */
-const payonIntegrator = (integrationMode) => {
-  switch (integrationMode) {
-    case 'CopyandPay':
-      console.log('Oh hey, cnp!')
-      break
-    default:
-      console.log('ahoy!')
-      break
-  }
+const integrator = {
+  /**
+   * submits your data to generate a checkout ID
+   * @param {boolean} isTestMode determines if you want to hit the test or live environments
+   * @param {string} accessToken
+   * @param {string} parameters
+   * @returns promise json
+   */
+  async submitCopyandPay(isTestMode, accessToken, parameters) {
+    let endPoint = ''
+
+    // eval mode
+    if (isTestMode) {
+      endPoint = 'https://eu-test.oppwa.com/v1/checkouts'
+    } else {
+      endPoint = 'https://eu-prod.oppwa.com/v1/checkouts'
+    }
+
+    const rawResponse = await fetch(endPoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: parameters,
+    })
+
+    const response = await rawResponse.json()
+
+    return response
+  },
 }
 
-module.exports = { payonIntegrator }
+export default integrator
