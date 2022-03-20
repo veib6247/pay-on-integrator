6a934e1a5be7be63ed7e5f4bd53ade553f252ba0
Bryan Olandres <ozoneblacklight@outlook.com>
2022-03-20 20:51:15 +0800

    added support for CommonJS and ES Modules in build

--- dist/module.js
+++ dist/module.js
@@ -1,24 +1,11 @@
-var $iZ8Gi$nodefetch = require("node-fetch");
+import $42D0D$nodefetch from "node-fetch";
 
-function $parcel$interopDefault(a) {
-  return a && a.__esModule ? a.default : a;
-}
-function $parcel$defineInteropFlag(a) {
-  Object.defineProperty(a, '__esModule', {value: true, configurable: true});
-}
-function $parcel$export(e, n, v, s) {
-  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
-}
 
-$parcel$defineInteropFlag(module.exports);
-
-$parcel$export(module.exports, "default", () => $434b8e1abb85fd55$export$2e2bcd8739ae039);
-
-const $56e754897a5d3317$export$14800167e7893ba3 = (isTestMode)=>{
+const $4803ebcc7d901773$export$14800167e7893ba3 = (isTestMode)=>{
     if (isTestMode) return 'eu-test';
     else return 'eu-prod';
 };
-const $56e754897a5d3317$export$7e605cc38c3f1d09 = (errorMsg)=>{
+const $4803ebcc7d901773$export$7e605cc38c3f1d09 = (errorMsg)=>{
     console.error(errorMsg) // Display log to console in case data isn't being captured.
     ;
     return errorMsg;
@@ -27,7 +14,7 @@ const $56e754897a5d3317$export$7e605cc38c3f1d09 = (errorMsg)=>{
 
 /**
  * main object to handle most of the operations
- */ const $434b8e1abb85fd55$var$integrator = {
+ */ const $ad4c169ceb31f1fd$var$integrator = {
     /**
    * Submits the data to the appropriate endpoint depending on the integrationType provided by the user.
    *
@@ -50,16 +37,16 @@ const $56e754897a5d3317$export$7e605cc38c3f1d09 = (errorMsg)=>{
         let endPoint = '';
         switch(integrationType){
             case 'CopyAndPay':
-                endPoint = `https://${$56e754897a5d3317$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/checkouts`;
+                endPoint = `https://${$4803ebcc7d901773$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/checkouts`;
                 break;
             case 'ServerToServer':
-                endPoint = `https://${$56e754897a5d3317$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/payments`;
+                endPoint = `https://${$4803ebcc7d901773$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/payments`;
                 break;
             case 'threeDSecure':
-                endPoint = `https://${$56e754897a5d3317$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/threeDSecure`;
+                endPoint = `https://${$4803ebcc7d901773$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/threeDSecure`;
                 break;
             case 'TokenizeStandAlone':
-                endPoint = `https://${$56e754897a5d3317$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/registrations`;
+                endPoint = `https://${$4803ebcc7d901773$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/registrations`;
                 break;
             case 'Manage':
                 // eval if user passed a referenceId
@@ -67,7 +54,7 @@ const $56e754897a5d3317$export$7e605cc38c3f1d09 = (errorMsg)=>{
                     let error = `"referenceId" is required for Integration Type "Manage".`;
                     console.error(error);
                     return error;
-                } else endPoint = `https://${$56e754897a5d3317$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/payments/${referenceId}`;
+                } else endPoint = `https://${$4803ebcc7d901773$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/payments/${referenceId}`;
                 break;
             case 'Recurring':
                 // eval if user passed a referenceId
@@ -75,16 +62,16 @@ const $56e754897a5d3317$export$7e605cc38c3f1d09 = (errorMsg)=>{
                     let error = `"referenceId" is required for Integration Type "Recurring".`;
                     console.error(error);
                     return error;
-                } else endPoint = `https://${$56e754897a5d3317$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/registrations/${referenceId}/payments`;
+                } else endPoint = `https://${$4803ebcc7d901773$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/registrations/${referenceId}/payments`;
                 break;
             // display error to user in case type is not within scope
             default:
-                return $56e754897a5d3317$export$7e605cc38c3f1d09(`The Integration Type "${integrationType}" is not recognized. Please choose only from the following: ["CopyAndPay", "ServerToServer", "threeDSecure", "TokenizeStandAlone", "Manage"]`);
+                return $4803ebcc7d901773$export$7e605cc38c3f1d09(`The Integration Type "${integrationType}" is not recognized. Please choose only from the following: ["CopyAndPay", "ServerToServer", "threeDSecure", "TokenizeStandAlone", "Manage"]`);
         }
         // Hit the API, catch errors just in case.
         try {
             // fetch
-            const rawResponse = await ($parcel$interopDefault($iZ8Gi$nodefetch))(endPoint, {
+            const rawResponse = await $42D0D$nodefetch(endPoint, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
@@ -111,21 +98,21 @@ const $56e754897a5d3317$export$7e605cc38c3f1d09 = (errorMsg)=>{
         let endPoint = '';
         switch(idType){
             case 'checkoutId':
-                endPoint = `https://${$56e754897a5d3317$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/checkouts/${id}/payment?entityId=${entityId}`;
+                endPoint = `https://${$4803ebcc7d901773$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/checkouts/${id}/payment?entityId=${entityId}`;
                 break;
             case 'paymentId':
-                endPoint = `https://${$56e754897a5d3317$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/query/${id}?entityId=${entityId}`;
+                endPoint = `https://${$4803ebcc7d901773$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/query/${id}?entityId=${entityId}`;
                 break;
             case 'merchantTransactionId':
-                endPoint = `https://${$56e754897a5d3317$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/query?entityId=${entityId}&merchantTransactionId=${id}`;
+                endPoint = `https://${$4803ebcc7d901773$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/query?entityId=${entityId}&merchantTransactionId=${id}`;
                 break;
             // error handling for unsupported id type
             default:
-                return $56e754897a5d3317$export$7e605cc38c3f1d09(`ID Type "${idType}" is not recognized. Please choose from ["checkoutId", "paymentId", "merchantTransactionId"] only.`);
+                return $4803ebcc7d901773$export$7e605cc38c3f1d09(`ID Type "${idType}" is not recognized. Please choose from ["checkoutId", "paymentId", "merchantTransactionId"] only.`);
         }
         // fetch but GET method this time
         try {
-            const rawResponse = await ($parcel$interopDefault($iZ8Gi$nodefetch))(endPoint, {
+            const rawResponse = await $42D0D$nodefetch(endPoint, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
@@ -147,10 +134,10 @@ const $56e754897a5d3317$export$7e605cc38c3f1d09 = (errorMsg)=>{
    * @returns
    */ deleteRegistration: async (accessToken, entityId, registrationID, isTestMode = true)=>{
         // init endpoint
-        const endPoint = `https://${$56e754897a5d3317$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/registrations/${registrationID}?entityId=${entityId}`;
+        const endPoint = `https://${$4803ebcc7d901773$export$14800167e7893ba3(isTestMode)}.oppwa.com/v1/registrations/${registrationID}?entityId=${entityId}`;
         // fetch but GET method this time
         try {
-            const rawResponse = await ($parcel$interopDefault($iZ8Gi$nodefetch))(endPoint, {
+            const rawResponse = await $42D0D$nodefetch(endPoint, {
                 method: 'DELETE',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
@@ -164,7 +151,8 @@ const $56e754897a5d3317$export$7e605cc38c3f1d09 = (errorMsg)=>{
         }
     }
 };
-var $434b8e1abb85fd55$export$2e2bcd8739ae039 = $434b8e1abb85fd55$var$integrator;
+var $ad4c169ceb31f1fd$export$2e2bcd8739ae039 = $ad4c169ceb31f1fd$var$integrator;
 
 
-//# sourceMappingURL=main.js.map
+export {$ad4c169ceb31f1fd$export$2e2bcd8739ae039 as default};
+//# sourceMappingURL=module.js.map
